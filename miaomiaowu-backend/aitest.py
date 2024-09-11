c030d4605ad8855701c1cad145303880e09d97e4
周旭 <zhouxusoft@gmail.com>
2024-09-11 15:00:27 +0800

    ..

--- miaomiaowu-backend/aitest.py
+++ miaomiaowu-backend/aitest.py
@@ -1,13 +1,18 @@
 import requests
 import json
+import time
 
 url = "https://spark-api-open.xf-yun.com/v1/chat/completions"
 data = {
         "model": "generalv3.5", # 指定请求的模型
         "messages": [
             {
+                "role": "system",
+                "content": "根据用户提供的影视作品名称（如动漫、电视剧、电影等），用你所知的为用户提供该部作品的简介。你无需回答多余内容，仅返回简介正文即可"
+            },
+            {
                 "role": "user",
-                "content": "为斗破苍穹动漫写一个简短的简介"
+                "content": "斗破苍穹"
             }
         ],
    		"stream": True
@@ -19,6 +24,12 @@ response = requests.post(url, headers=header, json=data, stream=True)
 
 # 流式响应解析示例
 response.encoding = "utf-8"
+
+def print_content(content):
+    for i in range(0, len(content)):
+        print(content[i], end='', flush=True)
+        time.sleep(0.02)
+
 for line in response.iter_lines(decode_unicode="utf-8"): # type: ignore
     # print(repr(line))
     if line == 'data: [DONE]':
@@ -31,8 +42,6 @@ for line in response.iter_lines(decode_unicode="utf-8"): # type: ignore
         answer = answer.replace('\\n\\n', '\n')
         answer = answer.replace('\\n', '\n')
         answer = answer.replace('**', '')
-        print(answer, end='')
+        print_content(answer)
+        # print(answer, end='')
         # print(repr(answer))
-        
-    
-    
