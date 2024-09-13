a595163ce48457b89fcb735870fbe4568b6c2c00
周旭 <zhouxusoft@gmail.com>
2024-09-13 16:27:06 +0800

    。。

--- miaomiaowu-frontend/components/watchitem/watchitem.js
+++ miaomiaowu-frontend/components/watchitem/watchitem.js
@@ -7,7 +7,27 @@ Component({
 	properties: {
 		watchitemname: {
 			type: String,
-			value: '斗破苍穹年番'
+			value: '斗破苍穹年番斗破苍穹年番'
+		},
+		watchitemmade: {
+			type: String,
+			value: '幻维数码'
+		},
+		watchwatchmode: {
+			type: String,
+			value: '连载中'
+		},
+		watchupdateto: {
+			type: String,
+			value: '110'
+		},
+		watchwatchto: {
+			type: String,
+			value: '102'
+		},
+		watchsummary: {
+			type: String,
+			value: '三年之约后，萧炎终于在迦南学院见到了薰儿，此后他广交挚友并成立磐门；为继续提升实力以三上云岚宗为父复仇，他以身犯险深入天焚炼气塔吞噬陨落心炎'
 		}
 	},
 
