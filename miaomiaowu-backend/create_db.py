04bc3d0af51c205c4bd92dbb89beef7f444dffb0
周旭 <zhouxusoft@gmail.com>
2024-09-24 02:16:58 +0800

    。。

--- miaomiaowu-backend/create_db.py
+++ miaomiaowu-backend/create_db.py
@@ -47,3 +47,35 @@ def create_database():
         ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;"
     )
 
+    dbcursor.execute("CREATE TABLE IF NOT EXISTS `user_level`  (\
+        `id` int NOT NULL AUTO_INCREMENT,\
+        `user_id` int NOT NULL,\
+        `experience` int NOT NULL DEFAULT 0 COMMENT '经验值',\
+        `ikun_level` int NOT NULL DEFAULT 1 COMMENT '1 初级小黑子，2 中级小黑子，3 高级小黑子， 4 哎呦你干嘛',\
+        PRIMARY KEY (`id`) USING BTREE\
+        ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;"
+    )
+
+    # platformList: ['腾讯视频', '哔哩哔哩', '爱奇艺', '优酷视频', '其它']
+    # updateModes: ['连载中', '已完结', '未更新']
+    # updateTime: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+    # 需要根据 update_number 和 set_time 以及 update_time 进行判断当前时间更新到哪一集
+    dbcursor.execute("CREATE TABLE IF NOT EXISTS `drama`  (\
+        `id` int NOT NULL AUTO_INCREMENT,\
+        `user_id` int NOT NULL,\
+        `drama_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+        `cover_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+        `introduction` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,\
+        `made_company` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,\
+        `playing_platform` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '2' COMMENT '存数组，例[0, 3] 于腾讯视频和优酷视频播放',\
+        `is_update` int NOT NULL COMMENT '0 已完结，1 连载中，2未更新',\
+        `total_number` int NOT NULL DEFAULT 0,\
+        `update_number` int NOT NULL DEFAULT 0,\
+        `set_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\
+        `update_time` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '存数组， 例[1, 6] 每周二周日更新',\
+        `love` float NOT NULL DEFAULT 0,\
+        `remark` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL,\
+        PRIMARY KEY (`id`) USING BTREE\
+        ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;"
+    )
+
